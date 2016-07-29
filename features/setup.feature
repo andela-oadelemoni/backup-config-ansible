7984616108384f5409eb4dc987cc04696f366e7a
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-07-29 01:25:19 +0100

    [rsnapshot] setup rsnapshot with passing cucumber test

---
+++ features/setup.feature
@@ -0,0 +1,20 @@
+Feature: Install rsnapshot backup tool and configure it to back up critical data
+	
+	Background:
+		Given that I have a running server
+		And I provision it
+
+	Scenario:
+		When I install rsnapshot
+		Then it should be successful
+		And the config file should exist
+
+	Scenario:
+		When I configure rsnapshot
+		Then it should be successful
+		And running configtest should return OK
+
+	Scenario:
+		When I setup backup cron jobs
+		Then it should be successful
+		And crontab should contain my list of jobs
---
+++ features/step_definitions/setup_steps.rb
@@ -0,0 +1,56 @@
+require 'open3'
+
+
+Given(/^that I have a running server$/) do
+  output, error, status = Open3.capture3 "vagrant reload"
+  expect(status.success?).to eq(true)
+end
+
+Given(/^I provision it$/) do
+  output, error, status = Open3.capture3 "vagrant provision"
+  expect(status.success?).to eq(true)
+end
+
+When(/^I install rsnapshot$/) do
+  command = "ansible-playbook -i playbooks/hosts.ini playbooks/rsnapshot.yml --tags 'install'"
+  output, error, @status = Open3.capture3 "#{command}"
+end
+
+Then(/^it should be successful$/) do
+  expect(@status.success?).to eq(true)
+end
+
+And(/^the config file should exist$/) do
+  expect(@status.success?).to eq(true)
+end
+
+When(/^I configure rsnapshot$/) do
+  command = "ansible-playbook -i playbooks/hosts.ini playbooks/rsnapshot.yml --tags 'configure'"
+  output, error, @status = Open3.capture3 "#{command}"
+end
+
+And(/^running configtest should return OK$/) do
+  command = "ansible-playbook -i playbooks/hosts.ini playbooks/rsnapshot.yml --tags 'configuration-test'"
+  output, error, status = Open3.capture3 "#{command}"
+
+  expect(status.success?).to eq(true)
+end
+
+When(/^I setup backup cron jobs$/) do
+  command = "ansible-playbook -i playbooks/hosts.ini playbooks/rsnapshot.yml --tags 'cron-setup'"
+  output, error, @status = Open3.capture3 "#{command}"
+end
+
+Then(/^crontab should contain my list of jobs$/) do
+  command = "ansible-playbook -i playbooks/hosts.ini playbooks/rsnapshot.yml --tags 'cron-check'"
+  output, error, status = Open3.capture3 "#{command}"
+
+  expect(status.success?).to eq(true)
+end
+
+### Additional piece of code
+
+# command = "ansible vagrant -i playbooks/hosts.ini -s -u vagrant -m stat -a 'path=/etc/rsnapshot.conf' | grep true"
+  # output, error, status = Open3.capture3 "#{command}"
+  # puts output
+  # puts error
